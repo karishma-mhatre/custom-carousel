0fc33b83b5063840f436bc105d85d17885d6e80b
Karishma Mhatre <karishmamhatre077@gmail.com>
2019-06-30 22:20:27 +0530

    Updates

---
+++ precache-manifest.c8cd79c9e24831fb2476b6eaa6138f36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3538369d909375865752bd9ef0a9d437",
+    "url": "/custome-carousel/index.html"
+  },
+  {
+    "revision": "4c9d498da77cdc3cf820",
+    "url": "/custome-carousel/static/css/main.229c2084.chunk.css"
+  },
+  {
+    "revision": "575c7ec77b85192a1af6",
+    "url": "/custome-carousel/static/js/2.49a16226.chunk.js"
+  },
+  {
+    "revision": "4c9d498da77cdc3cf820",
+    "url": "/custome-carousel/static/js/main.43659b19.chunk.js"
+  },
+  {
+    "revision": "c6d1c47da330662a8f05",
+    "url": "/custome-carousel/static/js/runtime~main.a8878fda.js"
+  }
+]);
